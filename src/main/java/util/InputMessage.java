b451edc0531c0a5654010984c1c1290219feb256
lee0jae330 <xingxing2001@cau.ac.kr>
2023-11-11 19:28:58 +0900

    refactor: 출력 문구 삭제

--- src/main/java/util/InputMessage.java
+++ src/main/java/util/InputMessage.java
@@ -1,7 +1,6 @@
 package util;
 
 public enum InputMessage {
-    PROGRAM_START_MSG(12,"안녕하세요! 우테코 식당 %d월 이벤트 플래너입니다."),
     ENTER_DATE(12,"%d월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
     ENTER_MENU("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
 
