eb94313b46f279a949d076033b2c34d7d532f55e
lee0jae330 <xingxing2001@cau.ac.kr>
2023-11-11 19:45:14 +0900

    feat: String.format 사용을 위해 달을 나타내는 상수 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,20 +2,23 @@ package view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import util.ConstOfPlanner;
 import util.InputMessage;
+
 import view.validator.InputValidator;
 
 public class InputView {
+    private static final int MONTH = ConstOfPlanner.MONTH.getNumber();
     private static final InputValidator inputValidator = new InputValidator();
 
     public static String enterDate() {
-        System.out.println(InputMessage.ENTER_DATE.getMessage());
+        System.out.println(InputMessage.ENTER_DATE.getMessage(MONTH));
         String input = Console.readLine();
         return inputValidator.checkInputValidation(input);
     }
 
     public static String enterMenu() {
-        System.out.println(InputMessage.ENTER_MENU.getMessage());
+        System.out.println(InputMessage.ENTER_MENU.getMessage(MONTH));
         String input = Console.readLine();
         return  inputValidator.checkInputValidation(input);
     }
