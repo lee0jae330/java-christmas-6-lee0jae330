7fd0d3c9ad264434d429b1230b0ab93c271b6296
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2023-11-11 14:53:07 +0000

    refactor: domain에서 유효성 검사를 하도록 변경

--- src/main/java/controller/PlannerController.java
+++ src/main/java/controller/PlannerController.java
@@ -1,7 +1,5 @@
 package controller;
 
-import controller.validator.DateValidator;
-
 import domain.VisitDate;
 
 import view.InputView;
@@ -9,28 +7,19 @@ import view.OutputView;
 
 public class PlannerController {
 
-    private final DateValidator dateValidator = new DateValidator();
-    private VisitDate visitDate;
-
     public void run() {
         OutputView.printStartMessage();
-        initDate();
+        VisitDate visitDate = initVisitDate();
+        
     }
 
-    private void initDate() {
-        String input = InputView.enterDate();
-        checkDateValidation(input);
-    }
-
-    private void checkDateValidation(String input) {
+    private VisitDate initVisitDate() {
         try {
-            int date = dateValidator.checkDateValidation(input);
-            visitDate = new VisitDate(date);
+            VisitDate visitDate = new VisitDate(InputView.enterDate());
+            return visitDate;
         } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-            initDate();
+            OutputView.printError(e.getMessage());
+            return initVisitDate();
         }
     }
-
-    //TODO: 메뉴 입력 받기 및 각종 혜택 적용 여부 확인 기능 추가
 }
