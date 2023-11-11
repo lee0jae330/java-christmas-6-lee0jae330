54a526baaea19a7bf9f6a9bdbef8ce91522e1a3d
lee0jae330 <xingxing2001@cau.ac.kr>
2023-11-11 20:03:47 +0900

    feat: 사용자로부터 날짜를 입력 받는 기능 추가

--- src/main/java/controller/PlannerController.java
+++ src/main/java/controller/PlannerController.java
@@ -1,10 +1,34 @@
 package controller;
 
+import controller.validator.DateValidator;
+
+import domain.VisitDate;
+
+import view.InputView;
 import view.OutputView;
 
 public class PlannerController {
+
+    private final DateValidator dateValidator = new DateValidator();
+    private VisitDate visitDate;
+
     public void run() {
         OutputView.printStartMessage();
 
+    }
+
+    private void initDate() {
+        String input = InputView.enterDate();
+        checkDateValidation(input);
+    }
+
+    private void checkDateValidation(String input) {
+        try {
+            int date = dateValidator.checkDateValidation(input);
+            visitDate = new VisitDate(date);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            initDate();
+        }
     }
 }
