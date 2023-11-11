45ee32fbde274964340dba818b4970633d8762ea
lee0jae330 <xingxing2001@cau.ac.kr>
2023-11-11 19:44:11 +0900

    feat: 동적으로 메세지 출력을 위해 String.format사용

--- src/main/java/util/InputMessage.java
+++ src/main/java/util/InputMessage.java
@@ -1,22 +1,16 @@
 package util;
 
 public enum InputMessage {
-    ENTER_DATE(12,"%d월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
+    ENTER_DATE("%d월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
     ENTER_MENU("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
 
     private final String message;
-    private final int month;
-    InputMessage(int month,String message) {
-        this.month = month;
-        this.message = message;
-    }
 
     InputMessage(String message) {
-        this.month = -1;
         this.message = message;
     }
 
-    public String getMessage() {
-        return message;
+    public String getMessage(int month) {
+        return String.format(message,month);
     }
 }
--- src/main/java/util/OutputMessage.java
+++ src/main/java/util/OutputMessage.java
@@ -1,16 +1,14 @@
 package util;
 
 public enum OutputMessage {
-    PROGRAM_START_MSG(12,"안녕하세요! 우테코 식당 %d월 이벤트 플래너입니다.");
-    private final int month;
+    PROGRAM_START_MSG("안녕하세요! 우테코 식당 %d월 이벤트 플래너입니다.");
     private final String message;
 
-    OutputMessage(int month, String message) {
-        this.month = month;
+    OutputMessage(String message) {
         this.message = message;
     }
 
-    public String getMessage() {
-        return message;
+    public String getMessage(int month) {
+        return String.format(message,month);
     }
 }
