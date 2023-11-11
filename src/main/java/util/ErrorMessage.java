2cf9bff9b8e7d5e2e047b46db3849caeb1100afd
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2023-11-11 13:47:54 +0000

    에러 메시지를 관리하는 enum클래스 생성

---
+++ src/main/java/util/ErrorMessage.java
@@ -0,0 +1,17 @@
+package util;
+
+public enum ErrorMessage {
+    INCORRECT_DATE("유효하지 않은 날짜 입니다. 다시 입력해 주세요."),
+    INCORRECT_ORDER("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    
+    private static final String ERROR = "[ERROR]";
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return ERROR + " " + message;
+    }
+}
