420d3711293d011255e699347db85eb83cd83939
lee0jae330 <xingxing2001@cau.ac.kr>
2023-11-11 19:45:27 +0900

    feat: String.format 사용을 위해 달을 나타내는 상수 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,5 +1,12 @@
 package view;
 
+import util.ConstOfPlanner;
+import util.OutputMessage;
+
 public class OutputView {
-    
+    private static final int MONTH = ConstOfPlanner.MONTH.getNumber();
+
+    public static void printStartMessage() {
+        System.out.println(OutputMessage.PROGRAM_START_MSG.getMessage(MONTH));
+    }
 }
