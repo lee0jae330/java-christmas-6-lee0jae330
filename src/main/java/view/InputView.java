d678a80f6a503519cd3dfb77216661ddb78abc1e
lee0jae330 <xingxing2001@cau.ac.kr>
2023-11-11 19:28:05 +0900

    feat: 사용자 입력 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,22 @@
 package view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import util.InputMessage;
+import view.validator.InputValidator;
+
 public class InputView {
-    
+    private static final InputValidator inputValidator = new InputValidator();
+
+    public static String enterDate() {
+        System.out.println(InputMessage.ENTER_DATE.getMessage());
+        String input = Console.readLine();
+        return inputValidator.checkInputValidation(input);
+    }
+
+    public static String enterMenu() {
+        System.out.println(InputMessage.ENTER_MENU.getMessage());
+        String input = Console.readLine();
+        return  inputValidator.checkInputValidation(input);
+    }
 }
