70d4e6865921b3f415c8c5653de596dcf1245ce1
lee0jae330 <xingxing2001@cau.ac.kr>
2023-11-12 14:34:16 +0900

    feat: 날짜와 주문 입력에 따라 다른 메서드로 유효성 검사

--- src/main/java/view/validator/InputValidator.java
+++ src/main/java/view/validator/InputValidator.java
@@ -1,14 +1,28 @@
 package view.validator;
 
+import exception.InvalidDateFormatException;
+
+
 public class InputValidator {
-    public String checkInputValidation(String input) {
-        checkInputIsEmpty(input);
+    public String checkDateValidation(String input) {
+        checkDateIsEmpty(input);
         return input;
     }
 
-    private void  checkInputIsEmpty(String input) {
+    public String checkOrderValidation(String input) {
+        checkOrderIsEmpty(input);
+        return input;
+    }
+
+    private void checkOrderIsEmpty(String input) {
         if(input.isEmpty()) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkDateIsEmpty(String input) {
+        if(input.isEmpty()) {
+            throw new InvalidDateFormatException();
         }
     }
 }
