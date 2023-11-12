751debcf39379bdf26de3549c7bc6131e6e2c605
lee0jae330 <xingxing2001@cau.ac.kr>
2023-11-12 14:03:39 +0900

    feat: 프로그램 시작 시 메뉴 목록 초기화 추가

--- src/main/java/controller/PlannerController.java
+++ src/main/java/controller/PlannerController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import domain.MenuNameDB;
 import domain.VisitDate;
 
 import view.InputView;
@@ -8,11 +9,11 @@ import view.OutputView;
 public class PlannerController {
 
     public void run() {
+        MenuNameDB menuNameDB = initMenuDB();
         OutputView.printStartMessage();
         VisitDate visitDate = initVisitDate();
-        
-    }
 
+    }
     private VisitDate initVisitDate() {
         try {
             VisitDate visitDate = new VisitDate(InputView.enterDate());
@@ -21,5 +22,11 @@ public class PlannerController {
             OutputView.printError(e.getMessage());
             return initVisitDate();
         }
+    }
+
+    private MenuNameDB initMenuDB() {
+        MenuNameDB menuNameDB = new MenuNameDB();
+        menuNameDB.initDB();
+        return menuNameDB;
     }
 }
