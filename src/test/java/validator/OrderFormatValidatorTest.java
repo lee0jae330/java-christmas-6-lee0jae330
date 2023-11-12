9ce3d70057fb505a9685ad21807f6ba951c49d82
lee0jae330 <xingxing2001@cau.ac.kr>
2023-11-12 16:33:46 +0900

    test: 쉼표(,)로 1차 파싱된 입력에 대한 유효성 검사 테스트 추가

---
+++ src/test/java/validator/OrderFormatValidatorTest.java
@@ -0,0 +1,20 @@
+package validator;
+
+import exception.InvalidOrderFormatException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+class OrderFormatValidatorTest {
+
+    @DisplayName("잘못된 형식의 주문 형태 입력 시 예외 발생 (올바른 형식:메뉴-수량)")
+    @ParameterizedTest
+    @ValueSource(strings = {"a--c","-aa-","aa-","-a"})
+    void checkInvalidOrderFormat(String input) {
+        assertThatThrownBy(() -> OrderFormatValidator.checkOrderFormat(input))
+                .isInstanceOf(InvalidOrderFormatException.class);
+    }
+}
