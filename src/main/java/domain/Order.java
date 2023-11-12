0c8a9f59d937e5f553b3e99cebcaae81c2c7c72d
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2023-11-12 14:16:02 +0000

    feat: 생성자 안에 setter 추가

--- src/main/java/domain/Order.java
+++ src/main/java/domain/Order.java
@@ -2,7 +2,9 @@ package domain;
 
 import java.util.List;
 
+import validator.MenuNameValidator;
 import validator.OrderFormatValidator;
+import validator.QuantityValidator;
 
 public class Order {
     private String menu;
@@ -10,14 +12,17 @@ public class Order {
 
     public Order (String input) {
         List<String> order = OrderFormatValidator.checkOrderFormat(input);
+        setMenu(order.get(0));
+        setQuantity(order.get(1));
     }
 
-    private void setMenu(String menu) {
+    private void setMenu(String menuName) {
+        MenuNameValidator.checkMenuNameValidation(menuName);
         this.menu = menu;
     }
     
-    private void setQuantity(String quantity) {
-        this.quantity = quantity;
+    private void setQuantity(String number) {
+        this.quantity = QuantityValidator.checkQuantityValidation(number);
     }
 
 }
