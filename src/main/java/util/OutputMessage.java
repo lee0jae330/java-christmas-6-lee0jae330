0ae6de6e5ae38b70daad42d8bbe7741b9587e36b
lee0jae330 <xingxing2001@cau.ac.kr>
2023-11-14 20:09:09 +0900

    feat: 출력 메세지 추가

--- src/main/java/util/OutputMessage.java
+++ src/main/java/util/OutputMessage.java
@@ -1,14 +1,30 @@
 package util;
 
 public enum OutputMessage {
-    PROGRAM_START_MSG("안녕하세요! 우테코 식당 %d월 이벤트 플래너입니다.");
+    PROGRAM_START_MSG("안녕하세요! 우테코 식당 %d월 이벤트 플래너입니다."),
+    BENEFIT_PREVIEW("%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
+    ORDERED_MENU("<주문 메뉴>"),
+    TOTAL_PRICE_BEFORE_DISCOUNT("<할인 전 총주문 금액>"),
+    BENEFIT_LIST("<혜택 내역>"),
+    TOTAL_BENEFIT_PRICE("<총혜택 금액>"),
+    TOTAL_PRICE_AFTER_DISCOUNT("<할인 후 예상 결제 금액>"),
+    EVENT_BADGE("<%d월 이벤트 배지>");
+
     private final String message;
 
     OutputMessage(String message) {
         this.message = message;
     }
 
+    public String getMessage() {
+        return message;
+    }
+
     public String getMessage(int month) {
         return String.format(message,month);
+    }
+
+    public String getMessage(int month, int date) {
+        return String.format(message,month,date);
     }
 }
