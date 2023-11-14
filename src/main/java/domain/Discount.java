9def77360edd83658d3a36f764dece10a01f5094
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2023-11-14 13:29:19 +0000

    feat: 할인을 관리하는 클래스 추가

--- src/main/java/domain/Discount.java
+++ src/main/java/domain/Discount.java
@@ -1,4 +1,31 @@
 package domain;
 
+import util.Drink;
+
 public class Discount {
-}
+    private int christmasDday;
+    private int weekday;
+    private int weekend;
+    private int special;
+    boolean isGiveawayEvent;
+
+    public Discount() {
+        this.christmasDday = 0;
+        this.weekday = 0;
+        this.weekend = 0;
+        this.special = 0;
+        this.isGiveawayEvent = false;
+    }
+
+    public int calculateRealDiscountAmount() {
+        return christmasDday + weekday + weekend + special;
+    }
+
+    public int calculateTotalDiscount() {
+        int sum = christmasDday + weekday + weekend + special;
+        if(isGiveawayEvent) {
+            sum += Drink.CHAMPAGNE.getPrice();
+        }
+        return sum;
+    }
+ }
