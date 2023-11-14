cbd37b0ed16be8bf73cbd363fb90c7aaccdccfe0
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2023-11-14 13:52:48 +0000

    feat: 상수 추가
    각 요일에 해당하는 날짜들을 7로 나누면 요일별로 날짜를 알 수 있는 점을 활용해 해당 상수 추가

--- src/main/java/util/ConstOfPlanner.java
+++ src/main/java/util/ConstOfPlanner.java
@@ -6,7 +6,15 @@ public enum ConstOfPlanner {
     MAX_OF_DATE(31),
     DATE_OF_CHRISTMAS(25),
     MAX_NUM_OF_ORDER(20),
-    MIN_NUM_OF_ORDER(1);
+    MIN_NUM_OF_ORDER(1),
+    MINIMUM_DISCOUNT_AMOUNT(10000),
+    MONDAY_MODULO_BY_7(4),
+    TUESDAY_MODULO_BY_7(5),
+    WEDNESDAY_MODULO_BY_7(6),
+    THURSDAY_MODULO_BY_7(0),
+    FRIDAY_MODULO_BY_7(1),
+    SATURDAY_MODULO_BY_7(2),
+    SUNDAY_MODULO_BY_7(3);
 
     private final int number;
 
