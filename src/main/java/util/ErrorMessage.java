40cfb8ebeb908336405ccc6b19a8b468092b7df4
lee0jae330 <xingxing2001@cau.ac.kr>
2023-11-14 20:06:22 +0900

    style: 오타 수정

--- src/main/java/util/ErrorMessage.java
+++ src/main/java/util/ErrorMessage.java
@@ -1,7 +1,7 @@
 package util;
 
 public enum ErrorMessage {
-    INCORRECT_DATE("유효하지 않은 날짜 입니다. 다시 입력해 주세요."),
+    INCORRECT_DATE("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
     INCORRECT_ORDER("유효하지 않은 주문입니다. 다시 입력해 주세요.");
     
     private static final String ERROR = "[ERROR]";
