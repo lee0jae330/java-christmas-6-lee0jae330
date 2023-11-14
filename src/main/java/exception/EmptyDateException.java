c6b94e8e26619dd0b044c02485377533a360aa5f
lee0jae330 <xingxing2001@cau.ac.kr>
2023-11-14 19:04:17 +0900

    refactor: 중복되는 예외 추가로 삭제

--- src/main/java/exception/EmptyDateException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-import util.ErrorMessage;
-
-public class EmptyDateException extends DateException {
-    public EmptyDateException() {
-        super(ErrorMessage.INCORRECT_DATE.getMessage());
-    }
-}
--- src/main/java/exception/EmptyOrderException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-import util.ErrorMessage;
-
-public class EmptyOrderException extends OrderException {
-    public EmptyOrderException() {
-        super(ErrorMessage.INCORRECT_ORDER.getMessage());
-    }
-}
