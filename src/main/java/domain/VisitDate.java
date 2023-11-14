a3eed4e298cf30ec97952bbd69fa88b773023b94
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2023-11-14 13:52:06 +0000

    feat: 메서드 추가
    1. 평일인지 판단하는 메서드
    2. 크리스마스보다 해당 날짜가 이전인지 판단하는 메서드
    3. 주말인지 판단하는 메서드
    4. 별이 적용된 날인지 판단하는 메서드

--- src/main/java/domain/VisitDate.java
+++ src/main/java/domain/VisitDate.java
@@ -1,5 +1,6 @@
 package domain;
 
+import util.ConstOfPlanner;
 import validator.DateValidator;
 
 public class VisitDate {
@@ -11,5 +12,37 @@ public class VisitDate {
 
     public int getDate() {
         return date;
+    }
+
+    public boolean isWeekday() {
+        int moduloResultBy7 = date % 7;
+        if( moduloResultBy7 == ConstOfPlanner.SUNDAY_MODULO_BY_7.getNumber() ||
+            moduloResultBy7 == ConstOfPlanner.MONDAY_MODULO_BY_7.getNumber() || 
+            moduloResultBy7 == ConstOfPlanner.TUESDAY_MODULO_BY_7.getNumber() || 
+            moduloResultBy7 == ConstOfPlanner.WEDNESDAY_MODULO_BY_7.getNumber() ||
+            moduloResultBy7 == ConstOfPlanner.THURSDAY_MODULO_BY_7.getNumber()) {
+                return true;
+            }
+        return false;
+    }
+
+    public boolean beforeChristmasDay() {
+        return date <= ConstOfPlanner.DATE_OF_CHRISTMAS.getNumber();
+    }
+
+    public boolean isWeekend() {
+        int moduloResultBy7 = date % 7;
+        if(moduloResultBy7 == ConstOfPlanner.FRIDAY_MODULO_BY_7.getNumber() || moduloResultBy7 == ConstOfPlanner.SATURDAY_MODULO_BY_7.getNumber()) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isSpecialDay() {
+        int moduloResultBy7 =  date % 7;
+        if(moduloResultBy7 == ConstOfPlanner.SUNDAY_MODULO_BY_7.getNumber() || date == ConstOfPlanner.DATE_OF_CHRISTMAS.getNumber()) {
+            return true;
+        }
+        return false;
     }
 }
