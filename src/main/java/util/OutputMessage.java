a0fce401a31a981590a755c80e1e0bbcf7987743
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2023-11-14 14:56:16 +0000

    feat: 증정메뉴 문구 출력

--- src/main/java/util/OutputMessage.java
+++ src/main/java/util/OutputMessage.java
@@ -4,6 +4,7 @@ public enum OutputMessage {
     PROGRAM_START_MSG("안녕하세요! 우테코 식당 %d월 이벤트 플래너입니다."),
     BENEFIT_PREVIEW("%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
     ORDERED_MENU("<주문 메뉴>"),
+    GIVEAWAY_MENU("증정메뉴"),
     TOTAL_PRICE_BEFORE_DISCOUNT("<할인 전 총주문 금액>"),
     BENEFIT_LIST("<혜택 내역>"),
     TOTAL_BENEFIT_PRICE("<총혜택 금액>"),
