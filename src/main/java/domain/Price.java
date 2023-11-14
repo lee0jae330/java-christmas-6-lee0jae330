9b13f55c98958ca8ca352850359736823028e863
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2023-11-14 13:28:56 +0000

    feat: 금액에 따라 할인 적용 여부 확인 메서드 추가

--- src/main/java/domain/Price.java
+++ src/main/java/domain/Price.java
@@ -1,11 +1,18 @@
 package domain;
 
+import util.ConstOfPlanner;
+
 public class Price {
     private final int totalPriceBeforeDiscount;
-    private int totalPriceAfterDiscount;
+    private final int totalPriceAfterDiscount;
 
     public Price(int price) {
         this.totalPriceBeforeDiscount = price;
+        this.totalPriceAfterDiscount = price;
+    }
+
+    public boolean checkDiscoutEligibility() {
+        return totalPriceBeforeDiscount >= ConstOfPlanner.MINIMUM_DISCOUNT_AMOUNT.getNumber();
     }
 
     public int getTotalPriceBeforeDiscount() {
