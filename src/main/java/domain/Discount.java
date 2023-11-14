4d29570c8bc2b28b4fac2c30f4841f3e32fd0f36
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2023-11-14 14:20:29 +0000

    feat: 할인을 적용하는 메서드 추가
    1. 크리스마스 D-day 할인 적용
    2. 평일 할인 적용
    3. 주말 할인 적용
    4. 특별 할인 적용
    5. 증정 이벤트 적용
    6. 총 혜택 금액에 따라 뱃지 적용

--- src/main/java/domain/Discount.java
+++ src/main/java/domain/Discount.java
@@ -1,5 +1,6 @@
 package domain;
 
+import util.ConstOfPlanner;
 import util.Drink;
 
 public class Discount {
@@ -7,14 +8,16 @@ public class Discount {
     private int weekday;
     private int weekend;
     private int special;
-    boolean isGiveawayEvent;
-
+    private boolean isGiveawayEvent;
+    private String badge;
+    
     public Discount() {
         this.christmasDday = 0;
         this.weekday = 0;
         this.weekend = 0;
         this.special = 0;
         this.isGiveawayEvent = false;
+        this.badge = "";
     }
 
     public int calculateRealDiscountAmount() {
@@ -27,5 +30,51 @@ public class Discount {
             sum += Drink.CHAMPAGNE.getPrice();
         }
         return sum;
+    }
+
+    public void adjustChristmasDdayDiscount(boolean beforeChristmasDay, int date) {
+        
+        if(beforeChristmasDay) {
+            int defaultAmount = ConstOfPlanner.DEFAULT_AMOUNT_OF_CHRISTMAS_DDAY_DISCOUNT.getNumber();
+            int additionalAmount =  ConstOfPlanner.ADDITIONAL_AMOUNT_OF_CHRISTMAS_DDAY_DISCOUNT.getNumber();
+            christmasDday = defaultAmount + additionalAmount * (date -1);
+        }
+    }    
+    
+    public void adjustWeekdayDiscount(boolean isWeekday, int numberOfDessert) {
+        if(isWeekday) {
+            weekday += ConstOfPlanner.AMOUNT_OF_WEEKDAY_DISCOUNT.getNumber() * numberOfDessert;
+        }
+    }
+
+    public void adjustWeekendDiscount(boolean isWeekend, int numberOfMainDish) {
+        if(isWeekend) {
+            weekend += ConstOfPlanner.AMOUNT_OF_WEEKEND_DISCOUNT.getNumber() * numberOfMainDish;
+        }
+    }
+
+    public void adjustSpecialDiscount(boolean isSpecialDay) {
+        if(isSpecialDay) {
+            special += 1000;
+        }
+    }
+
+    public void adjustGiveawayEvent(int priceBeforeDiscount) {
+        if(priceBeforeDiscount >= ConstOfPlanner.MINIMUM_AMOUNT_OF_GIVEAWAY_EVENT.getNumber()) {
+            isGiveawayEvent = true;
+        }
+    }
+
+    public void analyzeBadge() {
+        int totalDiscount = calculateTotalDiscount();
+        if(totalDiscount >= 20000) {
+            badge = "산타";
+        }
+        else if(totalDiscount >= 10000) {
+            badge = "트리";
+        }
+        else if(totalDiscount >= 5000) {
+            badge = "별";
+        }
     }
  }
