fb37091c2b2e15c22a3e9604d2f62c10524292f1
leeyeongjae <xingxing2001@cau.ac.kr>
2023-11-15 15:59:47 +0900

    test: OrderInputFormatValidator 단위테스트 작성

--- src/test/java/validator/OrderInputFormatValidatorTest.java
+++ src/test/java/validator/OrderInputFormatValidatorTest.java
@@ -1,6 +1,7 @@
 package validator;
 
 import exception.InvalidOrderFormatException;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
